2edabc45ea5b110a12791c88a45f9ecc2a8db238
GarethHarcombe <gharcombe2001@outlook.com>
2019-08-21 15:24:39 +1200

    Added beginning of explanations subpage, may be useful when testing high fidelity features to see them on a different design

---
+++ explanations.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+		<meta  http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="style.css">
+		<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+		<title>Biology Explanations</title>
+		<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    </head>
+    <body>
+		<div class="warning">
+			<p>Welcome to Biology Simulation!<br>Please turn your phone to landscape mode by turning it sideways</p>
+		</div>
+
+		<div class="wrapper">
+			<div class="item">
+					The red squares are organisms, going about their lives trying to survive. Simply existing requires energy - each second, every organism will lose energy, with larger and faster organisms using more energy. If they run out of energy, then they will die. Therefore, in order to survive, they must gain energy by eating food - the green squares - or if they are big enough, other organisms! If they gain enough energy to reproduce, they will do so, creating one offspring. However, this offspring has a chance for random mutations to occur, either increasing or decreasing its speed, size and ability to sense food. If this provides an adaptive advantage then the offspring will be more likely to survive and reproduce. Over time, this results in the more favourable traits increasing in frequency.
+			</div>
+		</div>
+    </body>
+</html>
